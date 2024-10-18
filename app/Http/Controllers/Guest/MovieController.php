22c4ec01999d869f80ae8d25c26498fb9c7a7a49
claudiopatti <claudiopatti1997@gmail.com>
2024-10-18 09:16:49 +0200

    collegata tabella tramite .env e un model

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index(){
+        $movies = Movie::all();
+
         $firstName = 'Gino';
         $lastName = 'Paoli';
 
-        return view('welcome', [
+        return view('welcome', 'movies', [
             'firstName' => $firstName,
             'lastName' => $lastName,
         ]);
